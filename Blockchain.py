0a9bfef5aa779b82040a717b440d3b8d11dc4007
Yejin Min <pnudpwls1103@pusan.ac.kr>
2022-02-23 02:53:43 +0900

    클래스 수정, utxo, transaction 조회, 키 생성 추가

--- Blockchain.py
+++ Blockchain.py
@@ -4,28 +4,43 @@ import hashlib
 import json
 import requests
 
+from Block import Block
+from Transaction import Transaction
+
 
 class Blockchain:
     def __init__(self):
         self.chain = []  # 블록들의 리스트 => 블록체인
-        self.transactions = []
-        self.create_block(proof=1, previous_hash='0')
-        # 가장 첫 블록엔 '제네시스블록'을 생성하기 위해 proof를 1, previous_hash를 0으로 한다.
+        self.transaction_list = []
+        # 타겟 난이도 비트
+        self.bits = 2
         self.nodes = set()
 
-    def create_block(self, proof, previous_hash):
-        # json으로 구조화된 block을 생성하고
-        # chain에 append 해준 뒤 block을 리턴한다.
-        block = {
-            'index': len(self.chain),
-            'timestamp': str(datetime.datetime.now()),
-            'proof': proof,
-            'previous_hash': previous_hash,
-            'transactions': self.transactions
-        }
-        self.transactions = []
-        self.chain.append(block)
-        return block
+        genesis_nonce = self.proof_of_work(100)
+        genesis_block = Block(None, [], datetime.datetime.now(), self.bits, genesis_nonce)
+        genesis_block.generate_hash()
+        self.chain.append(json.loads(str(genesis_block)))
+
+    def make_transaction(self, amount, receiver_address, sender_address, sender_private_key):
+        transaction = Transaction(self, amount, receiver_address, sender_address, sender_private_key)
+        # try:
+        #     transaction = Transaction(self, amount, receiver_address, sender_address, sender_private_key)
+        # except Exception as e:
+        #     return str(e)
+        self.transaction_list.append(json.loads(str(transaction)))
+        return transaction.hash
+
+    def create_block(self, miner_address):
+        nonce = self.proof_of_work(self.get_previous_block()['nonce'])
+        tx_list = self.transaction_list
+        # 코인 베이스 트랜잭션
+        tx_list.insert(0, json.loads(str(Transaction(self, 20, miner_address))))
+        self.transaction_list = []
+        last_block = self.chain[-1]
+        new_block = Block(last_block['hash'], tx_list, datetime.datetime.now(), self.bits, nonce)
+        new_block.generate_hash()
+        self.chain.append(json.loads(str(new_block)))
+        return json.loads(str(new_block))
 
     def get_previous_block(self):
         return self.chain[-1]
@@ -46,10 +61,6 @@ class Blockchain:
                 new_proof += 1
 
         return new_proof
-
-    def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(encoded_block).hexdigest()
 
     def add_node(self, address):
         parsed_url = urlparse(address)
@@ -86,10 +97,9 @@ class Blockchain:
             # print('%s' % last_block)
             # print('%s' % block)
             # print("\n---------\n")
-            #블록의 해쉬값 확인
-            if block['previous_hash'] != self.hash(last_block):
+            # 블록의 해쉬값 확인
+            if block['previous_block'] != last_block['hash']:
                 return False
             last_block = block
             current_index += 1
         return True
-
--- main.py
+++ main.py
@@ -1,53 +1,105 @@
-from Mine import *
-from Transaction import *
-import time
-from flask import Flask, jsonify
-from flask import request
-import requests
+import json
+import random
+import bitcoin
+from flask import Flask, jsonify, request
+
+from Blockchain import Blockchain
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 b = Blockchain()
-m = Mine(b)
-t = Transaction(b)
 
 
+# blockchain 확인
 @app.route('/', methods=['GET'])
 def get_chain():
+    block_json = []
+    for block in b.chain:
+        if 'list' in str(type(block['transactions'])):
+            continue
+        transactions = json.loads(block['transactions'])
+        transaction_json = []
+        for transaction in transactions:
+            tx_input_json = []
+            for input in json.loads(transaction['tx_inputs']):
+                tx_input_json.append(input)
+            tx_output_json = []
+            for output in json.loads(transaction['tx_outputs']):
+                tx_output_json.append(output)
+            transaction['tx_inputs'] = tx_input_json
+            transaction['tx_outputs'] = tx_output_json
+            transaction_json.append(transaction)
+        block['transactions'] = transaction_json
+        block_json.append(block)
     response = {
-        'chain': b.chain,
+        'chain': block_json,
         'length': len(b.chain)
     }
     return response
 
 
+# 키 생성
+@app.route('/generatekey', methods=['GET'])
+def generate_key():
+    private_key = bitcoin.sha256(random.choice('something'))
+    public_key = bitcoin.privkey_to_pubkey(private_key)
+    address = bitcoin.pubkey_to_address(public_key)
+    info = {
+        'private_key': private_key,
+        'public_key': public_key,
+        'address': address
+    }
+    return info
+
+
+# 블록 마이닝
 @app.route('/mine', methods=['GET'])
 def mining():
-    block = m.mining()
+    request_json = request.get_json()
+    block = b.create_block(request_json.get('private_key'))
+    block['transactions'] = json.loads(block['transactions'])
+    transaction_json = []
+    for transaction in block['transactions']:
+        tx_input_json = []
+        for input in json.loads(transaction['tx_inputs']):
+            tx_input_json.append(input)
+        tx_output_json = []
+        for output in json.loads(transaction['tx_outputs']):
+            tx_output_json.append(output)
+        transaction['tx_inputs'] = tx_input_json
+        transaction['tx_outputs'] = tx_output_json
+        transaction_json.append(transaction)
+    block['transactions'] = transaction_json
     responses = {
         'message': 'Congrate! you mined a block!',
-        **block
+        'block': block,
     }
     return responses
 
 
 @app.route('/add_transaction', methods=['GET'])
 def add_transaction():
-    json = request.get_json()
-    transaction_keys = ['sender', 'receiver', 'amount']
-    if not all(key in json for key in transaction_keys):
+    request_json = request.get_json()
+    transaction_keys = ['amount', 'receiver', 'sender', 'senderPrivate']
+    if not all(key in request_json for key in transaction_keys):
         return 'Some elements of the transaction are missing', 400
-    index = t.add_transaction(
-        sender=json['sender'],
-        receiver=json['receiver'],
-        amount=json['amount']
+    value = b.make_transaction(
+        amount=request_json['amount'],
+        receiver_address=request_json['receiver'],
+        sender_address=request_json['sender'],
+        sender_private_key=request_json['senderPrivate'],
     )
-    response = {
-        'message': f'This transaction will be added to Block {index}'
-    }
 
-    print(index)
+    if value == '잔액 부족':
+        response = {
+            'message': value
+        }
+    else:
+        response = {
+            'message': f'this transaction\'s hash value is {value}'
+        }
+
     return jsonify(response), 201
 
 
@@ -69,17 +121,31 @@ def connect_node():
 @app.route('/update_chain', methods=['GET'])
 def update_chain():
     is_chain_updated = b.update_chain()
+    chain_json = []
+    for i in range(len(b.chain)):
+        chain_json.append(b.chain[i])
     if is_chain_updated:
         response = {
             'message': 'The nodes had different chains so the chain was replacted by the longest chain.',
-            'new_chain': b.chain
+            'new_chain': chain_json
         }
     else:
         response = {
             'message': 'All good. The chain is the largest one.',
-            'actual_chain': b.chain
+            'actual_chain': chain_json
         }
     return jsonify(response), 200
+
+
+@app.route('/transactions', methods=['GET'])
+def return_transactions():
+    tx_json = []
+    tx_list = b.transaction_list
+    for tx in tx_list:
+        tx['tx_inputs'] = json.loads(tx['tx_inputs'])
+        tx['tx_outputs'] = json.loads(tx['tx_outputs'])
+        tx_json.append(tx)
+    return jsonify(tx_json), 201
 
 
 app.run(host='0.0.0.0', port=5000)
